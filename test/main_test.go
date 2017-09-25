2f5f0ad3bac56dd6dad31fada1f1c2afe7fbcbe1
Elliott Beach <elliott2.71828@gmail.com>
2017-09-25 00:12:57 -0500

    cleanup

---
+++ test/main_test.go
@@ -0,0 +1,10 @@
+package test
+
+import (
+	example "github.com/e-beach/main-package-example"
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	example.SampleFunction()
+}
--- test/test_main.go
+++
@@ -1,7 +0,0 @@
-package test
-
-import example "main-package-example"
-
-func Test(){
-	example.SampleFunction()
-}
